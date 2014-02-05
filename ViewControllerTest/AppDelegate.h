704757f5451847599f6743b6d64ed45525b618a9
jan@appzman.com <jan@appzman.com>
2014-02-05 09:39:13 -0600

    Initial Commit

---
+++ ViewControllerTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewControllerTest
+//
+//  Created by Janusz Chudzynski on 2/5/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewControllerTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControllerTest
+//
+//  Created by Janusz Chudzynski on 2/5/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
